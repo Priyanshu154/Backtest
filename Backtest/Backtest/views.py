f297d5ce5600bf86a2fb8057354651ecfe42ca6b
damletanmay <53563619+damletanmay@users.noreply.github.com>
2020-09-13 08:56:09 +0530

    "Nothing Much"

--- Backtest/Backtest/views.py
+++ Backtest/Backtest/views.py
@@ -4,9 +4,13 @@ def Home(request):
     return render(request,'homepage.html')
 
 def result(request):
+
     stock =request.GET["stock"]
     entry1=request.GET["entry_indicator1"]
     entry2=request.GET["entry_indicator2"]
     exit1=request.GET["exit_indicator1"]
     exit2=request.GET["exit_indicator2"]
-    return render(request,'result.html',{'stock':stock,'entry1':entry1,'entry2':entry2,'exit1':exit1,'exit2':exit2})
+
+    pro_lo = backtest(stock,entry1,exit1)
+
+    return render(request,'result.html',{'stock':stock,'entry1':entry1,'entry2':entry2,'exit1':exit1,'exit2':exit2,})
