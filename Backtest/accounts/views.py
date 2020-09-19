2bd337ed4fcf3be60e77958d487b9a6df97ca46f
damletanmay <53563619+damletanmay@users.noreply.github.com>
2020-09-19 13:59:16 +0530

    Added , Login, Logout, Signup System
    Also Added Welcome Page

--- Backtest/accounts/views.py
+++ Backtest/accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
