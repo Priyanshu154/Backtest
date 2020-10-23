3176a8895cc8a8f2efd18b80af2a993e39e2f8db
Darshan R Gupta <darshang631@gmail.com>
2020-10-23 15:53:17 +0530

    Javascript portion done
    All the parameters are ready

--- Backtest/Backtest/views.py
+++ Backtest/Backtest/views.py
@@ -10,15 +10,15 @@ def home(request):
 
 @login_required
 def result(request):
-    stock =request.GET["stock"]
+    stock =request.POST["stock"]
     entry = []
     para = []
     value = []
-    total_entry = request.GET.get("total_entries")
+    total_entry = request.POST.get("total_entries")
     for i in range( int(total_entry) ):
-        entry.append( request.GET.get("entry"+ str(i+1) ) )
-        para.append( request.GET.get("entry_parameter"+ str(i+1) ) )
-        value.append( request.GET.get("entry_value"+ str(i+1) ) )
+        entry.append( request.POST.get("entry"+ str(i+1) ) )
+        para.append( request.POST.get("entry_parameter"+ str(i+1) ) )
+        value.append( request.POST.get("entry_value"+ str(i+1) ) )
     print(stock)
     print(entry)
     print(para)
@@ -26,11 +26,11 @@ def result(request):
     para = []
     value =[]
     exit = []
-    total_exit = request.GET.get("total_exits")
+    total_exit = request.POST.get("total_exits")
     for i in range( int(total_exit) ):
-        exit.append( request.GET.get("exit"+ str(i+1) ) )
-        para.append( request.GET.get("exit_parameter"+ str(i+1) ) )
-        value.append( request.GET.get("exit_value"+ str(i+1) ) )
+        exit.append( request.POST.get("exit"+ str(i+1) ) )
+        para.append( request.POST.get("exit_parameter"+ str(i+1) ) )
+        value.append( request.POST.get("exit_value"+ str(i+1) ) )
     print(exit)
     print(para)
     print(value)
