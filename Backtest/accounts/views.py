143bd2b4a319bd8138a9474aa592944997c8b02c
damletanmay <53563619+damletanmay@users.noreply.github.com>
2020-10-27 10:27:56 +0530

    Polish Part-3
    made way many changes, too tired to exxplain them

--- Backtest/accounts/views.py
+++ Backtest/accounts/views.py
@@ -11,9 +11,12 @@ def signup(request):
                 user = User.objects.get(username = request.POST['username'])
                 return render(request,'accounts/signup.html',{'error':'WARNING !!! Username Already Exist.'})
             except User.DoesNotExist:
-                user = User.objects.create_user(username = request.POST['username'], password = request.POST['pass1'] )
-                auth.login(request,user)
-                return redirect('home')
+                if len(request.POST['pass1'])>8:
+                    user = User.objects.create_user(username = request.POST['username'], password = request.POST['pass1'] )
+                    auth.login(request,user)
+                    return redirect('home')
+                else:
+                    return render(request,'accounts/signup.html',{'error':'WARNING !!! Length of password should be greater than 8 letters.'})
         else:
             return render(request,'accounts/signup.html',{'error':'WARNING !!! Passwords Do Not Match.'})
     else:
